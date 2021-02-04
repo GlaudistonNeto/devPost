d1250bbba6202bb38fb9b266693e9eccabbbdb20
gneto <glaudistonson@gmail.com>
2021-02-04 19:52:57 -0300

    search view is done

--- src/screens/Search/index.js
+++ src/screens/Search/index.js
@@ -1,10 +1,61 @@
-import React from 'react';
-import { Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react';
+
+import SearchList from '../../components/SearchList';
+import firebase from '../../config/firebaseConfig';
+import { Feather } from '@expo/vector-icons';
+
+import { Container, AreaInput, Input, List } from './styles';
 
 export default function Search() {
+  const [input, setInput] = useState('');
+  const [users, setUsers] = useState('');
+
+  useEffect(() => {
+    if (input === ''|| input === undefined) {
+      setUsers([]);
+      return;
+    }
+
+    const subscriber = firebase.firestore().collection('users')
+      .where('name', '>=', input)
+      .where('name', '<=', input + '\uf8ff')
+      .onSnapshot(snapshot => {
+        const listsUsers= [];
+
+        snapshot.forEach(doc => {
+          listsUsers.push({
+            ...doc.data(),
+            id: doc.id
+          });
+        });
+        setUsers(listsUsers);
+        console.log(listsUsers);
+      });
+
+      return () => subscriber();
+  }, [input])
  return (
-   <View>
-     <Text>Search Screen</Text>
-   </View>
+   <Container>
+     <AreaInput>
+      <Feather
+        name="search"
+        color="#e52246"
+        size={20}
+      />
+      <Input
+        placeholder="Looking for someone?"
+        placeholderTextColor='#353840'
+        value={input}
+        onChangeText={(text) => setInput(text)}
+      />
+     </AreaInput>
+
+     <List
+       showsVerticalScrollIndicator={false}
+       data={users}
+       keyExtractor={(item) => item.id}
+       renderItem={({ item }) => <SearchList data={item} />}
+     />
+   </Container>
   );
 };
